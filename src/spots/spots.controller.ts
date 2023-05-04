e7c7a764c9776f956a48b9621827c5aa67bd950e
BartekStachowicz <bartoszstachowicz@gmail.com>
2023-05-04 14:35:15 +0200

    fishspot-backend

--- src/spots/spots.controller.ts
+++ src/spots/spots.controller.ts
@@ -1,13 +1,54 @@
-import { Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 import { SpotsService } from './spots.service';
+import { Spots, SpotsInfo } from './spots.model';
 
 @Controller('spots')
 export class SpotsController {
   constructor(private spotsService: SpotsService) {}
 
   @Post('regenerate-spotid/:lakename')
-  async regenerateSpotId(@Param('lakename') lakeName: string) {
+  async regenerateSpotId(@Param('lakename') lakeName: string): Promise<void> {
     await this.spotsService.regenerateSpotId(lakeName);
+  }
+
+  @Post('new-spot/:lakename')
+  async addNewSpot(
+    @Param('lakename') lakeName: string,
+    @Body() spot: Spots,
+  ): Promise<string> {
+    const id = await this.spotsService.addNewSpot(lakeName, spot);
+    return id;
+  }
+
+  @Post('update-spot/:lakename')
+  async updateSpot(
+    @Param('lakename') lakeName: string,
+    @Body() spot: Spots,
+  ): Promise<Spots> {
+    const updatedSpot = await this.spotsService.updateSpot(lakeName, spot);
+    return updatedSpot;
+  }
+
+  @Get('get-spot/:lakename/:id')
+  async getSpotById(
+    @Param('lakename') lakeName: string,
+    @Param('id') spotId: string,
+  ): Promise<{
+    spotId: string;
+    number: string;
+    info: SpotsInfo;
+  }> {
+    const spot = this.spotsService.getSpotById(lakeName, spotId);
+
+    return spot;
+  }
+
+  @Delete('delete-spot/:lakename/:id')
+  async deleteSpot(
+    @Param('lakename') lakeName: string,
+    @Param('id') spotId: string,
+  ) {
+    await this.spotsService.deleteSpot(lakeName, spotId);
   }
 }
--- src/spots/spots.service.ts
+++ src/spots/spots.service.ts
@@ -2,10 +2,73 @@ import { Injectable } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
 
 import { LakeService } from '../lake/lake.service';
+import { Spots, SpotsInfo } from './spots.model';
 
 @Injectable()
 export class SpotsService {
   constructor(private lakeService: LakeService) {}
+
+  async addNewSpot(lakeName: string, spot: Spots): Promise<string> {
+    const lake = await this.lakeService.findByName(lakeName);
+    const id = this.buildUniqueID(lakeName, spot.number);
+
+    const newSpot: Spots = {
+      ...spot,
+      spotId: id,
+    };
+
+    lake.spots.push(newSpot);
+
+    await this.lakeService.updateLake(lake);
+
+    return id;
+  }
+
+  async updateSpot(lakeName: string, spot: Spots): Promise<Spots> {
+    const lake = await this.lakeService.findByName(lakeName);
+    const spotIndex = lake.spots.findIndex((s) => s.spotId === spot.spotId);
+
+    if (spotIndex === -1) {
+      throw new Error('Spot not found');
+    }
+
+    const updatedSpot: Spots = {
+      ...lake.spots[spotIndex],
+      ...spot,
+    };
+
+    lake.spots.splice(spotIndex, 1, updatedSpot);
+
+    await this.lakeService.updateLake(lake);
+
+    return updatedSpot;
+  }
+
+  async deleteSpot(lakeName: string, spotId: string): Promise<void> {
+    const lake = await this.lakeService.findByName(lakeName);
+
+    lake.spots.filter((spot) => spot.spotId !== spotId);
+
+    await this.lakeService.updateLake(lake);
+  }
+
+  async getSpotById(
+    lakeName: string,
+    spotId: string,
+  ): Promise<{
+    spotId: string;
+    number: string;
+    info: SpotsInfo;
+  }> {
+    const lake = await this.lakeService.findByName(lakeName);
+    const spotOutput: Spots = lake.spots.find((spot) => spot.spotId === spotId);
+
+    return {
+      spotId: spotOutput.spotId,
+      number: spotOutput.number,
+      info: spotOutput.info,
+    };
+  }
 
   async regenerateSpotId(lakeName: string) {
     const lake = await this.lakeService.findByName(lakeName);
