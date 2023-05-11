2190b17443cf4303495bc5b81cb0a7fd04d3f777
BartekStachowicz <bartoszstachowicz@gmail.com>
2023-05-11 20:05:39 +0200

    fishspot-backend

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,18 @@ async function bootstrap() {
     origin: 'https://fish-spot.vercel.app',
     credentials: true,
   });
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+    );
+    next();
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 3000);
 }
