c5c8beccc5302ef3f56a5e3cf38a8bf7188f9462
BartekStachowicz <bartoszstachowicz@gmail.com>
2023-05-11 20:48:22 +0200

    fishspot-backend

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: 'https://fish-spot.vercel.app',
-    credentials: true,
+  app.enableCors();
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader(
+    //   "Access-Control-Allow-Headers",
+    //   "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    // );
+    // res.setHeader(
+    //   "Access-Control-Allow-Methods",
+    //   "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    // );
+    next();
   });
-  // app.use((req, res, next) => {
-  //   res.setHeader('Access-Control-Allow-Origin', '*');
-  //   res.setHeader(
-  //     'Access-Control-Allow-Headers',
-  //     'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  //   );
-  //   res.setHeader(
-  //     'Access-Control-Allow-Methods',
-  //     'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-  //   );
-  //   next();
-  // });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 3000);
 }
