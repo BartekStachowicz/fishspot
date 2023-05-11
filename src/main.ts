64642a1af3d83ac3ee3a96e814546344cf0ade4d
BartekStachowicz <bartoszstachowicz@gmail.com>
2023-05-11 21:15:17 +0200

    fishspot-backend

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,10 +8,17 @@ async function bootstrap() {
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-    allowedHeaders: ['content-type'],
+    allowedHeaders: [
+      'Access-Control-Allow-Headers',
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization',
+    ],
     preflightContinue: false,
   });
-
+  // app.use(cors());
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 3000);
 }
